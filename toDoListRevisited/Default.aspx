c04952bd2b524b3d5d4b89d5b6c3acbe50fb7670
wjlawlor <wjl28@msn.com>
2019-08-22 16:46:32 -0400

    All of the revisited toDoList Project.

---
+++ toDoListRevisited/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ToDoList.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>To Dos</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>    
+        <h1>To Dos</h1>
+        <uc:ListGeneration id="ListGeneration" runat="server" NumberOfRecordsToDisplay="3" CategoryFilter="Idea" />             
+
+        <h2>Add New To Do</h2>
+
+        <uc:DataEntry runat="server" />
+    </div>
+    <br />
+    <div>
+        <uc:ListGeneration id="ListGeneration1" runat="server" />   
+    </div>
+    </form>
+</body>
+</html>
---
+++ toDoListRevisited/Models/ToDoItemData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace toDoListRevisited.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ToDoItemData
+    {
+        // HACK: Using a static collection here
+        // to work around not having a database.
+        private static List<ToDoItem> Items = new List<ToDoItem>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static List<ToDoItem> GetToDoItems()
+        {
+            return Items;
+        }
+
+        public static void AddItem(string description, string category)
+        {
+            ToDoItem item = new ToDoItem();
+            item.Description = description;
+            item.Category = category;
+            item.Done = false;
+
+            Items.Add(item);
+        }
+    }
+}
